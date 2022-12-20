3a1990fd6f5a359a17bb72cc0a7f1c2840e32be0
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-12-20 18:35:04 +0000

    playing around

---
+++ app/routes/contact_us.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="sda">
+      <h1>Contact page</h1>
+    </div>
+  );
+}
---
+++ app/routes/demo-page.jsx
@@ -0,0 +1,7 @@
+export default function Demo() {
+  return (
+    <div className="sda">
+      <h1>Demo page</h1>
+    </div>
+  );
+}

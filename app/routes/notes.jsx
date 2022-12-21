82b535b9e4613dcdd378b06bd978491f51f18a34
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-12-21 16:19:57 +0000

    added fake backend

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function Notes() {
   return (
@@ -11,4 +13,24 @@ export default function Notes() {
 
 export function links() {
   return [...newNoteLinks()];
+}
+
+// any non get request sent will trigger
+export async function action({ request }) {
+  const formData = await request.formData();
+  // const noteData = Object.fromEntries(formData); can do it this way
+  const noteData = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+  };
+  // can add validation
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+
+  const updatedNotes = existingNotes.concat(noteData);
+
+  await storeNotes(updatedNotes);
+
+  console.log(updatedNotes);
+  return redirect("/notes");
 }
